699f0e6199a3fd0f898e58ae923a72ea04b60cfc
anatalibo <antonio.natali@unibo.it>
2020-07-17 19:10:02 +0200

    appdata

--- it.unibo.mirth.java/workingok/PERSON.sql
+++ it.unibo.mirth.java/workingok/PERSON.sql
@@ -1,4 +1,4 @@
 SET NAMES utf8mb4;
 
 INSERT INTO `PERSON` (`ID`, `USERNAME`, `FIRSTNAME`, `LASTNAME`, `ORGANIZATION`, `INDUSTRY`, `EMAIL`, `PHONENUMBER`, `DESCRIPTION`, `LAST_LOGIN`, `GRACE_PERIOD_START`, `STRIKE_COUNT`, `LAST_STRIKE_TIME`, `LOGGED_IN`) VALUES
-(1,	'admin',	'',	'',	'',	NULL,	'',	'',	'',	'2020-07-17 14:57:50',	NULL,	0,	NULL,	CONV('1', 2, 10) + 0);
+(1,	'admin',	'Antonio',	'Natali',	'DISI - University of Bologna',	'University',	'antonio.natali@unibo.it',	'',	'',	'2020-07-17 15:16:12',	NULL,	0,	NULL,	CONV('1', 2, 10) + 0);
