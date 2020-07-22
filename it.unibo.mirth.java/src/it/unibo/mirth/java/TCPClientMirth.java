cc454272d8a3a61c92e4632b23967324d2a768fe
anatalibo <antonio.natali@unibo.it>
2020-07-22 12:11:53 +0200

    more on mirth libs

--- it.unibo.mirth.java/src/it/unibo/mirth/java/TCPClientMirth.java
+++ it.unibo.mirth.java/src/it/unibo/mirth/java/TCPClientMirth.java
@@ -6,9 +6,9 @@ import java.net.*;
 
 class TCPClientMirth {
 	private Socket clientSocket = null ; 
-//	private String sentence ="testMsg";	
 
-	String sentence ="MSH|^~\\&|NES|NINTENDO|TESTSYSTEM|TESTFACILITY|20010101000000||ADT^A04|Q123456789T123456789X123456|P|2.3\r\n" + 
+	String sentence =
+	"MSH|^~\\&|NES|NINTENDO|TESTSYSTEM|TESTFACILITY|20010101000000||ADT^A04|Q123456789T123456789X123456|P|2.3\r\n" + 
 	"EVN|A04|20010101000000|||^KOOPA^BOWSER\r\n" + 
 	"PID|1||123456789|0123456789^AA^^JP|BROS^MARIO||19850101000000|M|||123 FAKE STREET^MARIO \\T\\ LUIGI BROS PLACE^TOADSTOOL KINGDOM^NES^A1B2C3^JP^HOME^^1234|1234|(555)555-0123^HOME^JP:1234567|||S|MSH|12345678|||||||0|||||N\r\n" + 
 	"NK1|1|PEACH^PRINCESS|SO|ANOTHER CASTLE^^TOADSTOOL KINGDOM^NES^^JP|(123)555-1234|(123)555-2345|NOK\r\n" + 
@@ -17,7 +17,8 @@ class TCPClientMirth {
 	"IN1|1|PAR^PARENT||||LUIGI\r\n" + 
 	"IN1|2|FRI^FRIEND||||PRINCESS";
 	
-	String sentence1 = "MSH|^~\\&|ADT1|SHM|SHMADT|SHM|200812091126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5|\r\n" + 
+	String sentence1 = 
+			"MSH|^~\\&|ADT1|SHM|SHMADT|SHM|200812091126|SECURITY|ADT^A01^ADT_A01|MSG00001|P|2.5|\r\n" + 
 			"EVN|A01|200812091126||\r\n" + 
 			"PID|1|1001|1001^5^M11^ADT1^MR^SHM~123456789^^^USSSA^SS||OHALLAHAN^COLLEENBOLOGNA^^||19850704|F||2106-3|1200 N ELM STREET^^NEWPORT BEACH^CA^92660-1020^US^H|OC|(949) 555-1234|(949) 555-5678||S||PATID1001^2^M10^ADT1^AN^A|123456789|U1234567^CA|\r\n" + 
 			"NK1|1|O'HALLAHAN^BRITTANY^M|SIS^SISTER||||N^NEXT-OF-KIN\r\n" + 
@@ -25,11 +26,11 @@ class TCPClientMirth {
 	
 	public void sendAMessage() {
 	try {
-	   System.out.println("sendAMessage: " + sentence1 );
+	   System.out.println("sendAMessage ... :\n " + sentence ); 
 	   clientSocket = new Socket("localhost", 7001); 
     //OUTPUT  
 	  DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
- 	  outToServer.writeBytes(sentence1 + '\n');  // + "\\n" 
+ 	  outToServer.writeBytes(sentence + '\n');  // + "\\n" 
 	  outToServer.flush();
 // 	  
 	}catch(Exception e) {
